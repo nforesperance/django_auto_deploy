60b0b3cc347b920608d1c6ef01d52818bdf39443
nfor <nforesperance>
2021-10-07 17:43:58 +0100

    self hosted6

--- django_auto_deploy/settings.py
+++ django_auto_deploy/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6&g#cr2+qh+0(lpv7**)ubmv_62o$6)x%s$8qm_g!&8rm++ag(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['51.79.31.146' 'localhost']
 
 
 # Application definition
