00aa0222d88e1d03bfdf463e81db1c8d2bf6ddcc
nfor <nforesperance>
2021-10-09 11:48:48 +0100

    test app added

--- django_auto_deploy/urls.py
+++ django_auto_deploy/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+from test_app.views import test
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',test,name='test_home')
 ]
 # if settings.DEBUG:
 #     urlpatterns += static(
